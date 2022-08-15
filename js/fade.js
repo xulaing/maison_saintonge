816e4e935b6ef74abf7940e831c330ebd612df11
xulaing <khauv@live.fr>
2022-08-15 23:23:59 +0200

    home animation

---
+++ js/fade.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#fade-logo').hide().delay(500).fadeIn(2000);
+    $('#home-footer').hide().delay(1500).fadeIn(2000);
+    $('#fade-logo').fadeOut(2000);
+});
+/*
+function footerColor() {
+    setTimeout(function () {
+        document.getElementById('white').style.color = "green";
+    }, 3000);
+};
+
+setTimeout(footerColor, 3000);*/
